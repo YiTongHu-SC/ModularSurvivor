9a27e44204c43072ac27b9d74b4a44552872381d
huyitong <huyitong@merryrealm.com>
2025-12-18 21:02:15 +0800

    update MainMenu

--- Assets/Scripts/UI/Menus/MainMenuView.cs
+++ Assets/Scripts/UI/Menus/MainMenuView.cs
@@ -1,9 +1,18 @@
 ﻿using UI.Framework;
+using UnityEngine.UI;
 
 namespace UI.Menus
 {
     public class MainMenuView : BaseView<MainMenuModelData>
     {
+        public Button StartButton;
+        public Button ExitButton;
+
+        protected override void InitializeView()
+        {
+            base.InitializeView();
+        }
+
         public override void UpdateView(MainMenuModelData data)
         {
         }
---
+++ Assets/Scripts/UI/Utils/UiTool.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace UI.Utils
+{
+    public static class UiTool
+    {
+        public static bool TryBind<T>(Transform source, string matchName, out T result) where T : MonoBehaviour
+        {
+            result = null;
+            foreach (var target in source.GetComponentsInChildren<T>())
+            {
+                if (target.name == matchName)
+                {
+                    result = target;
+                    break;
+                }
+            }
+
+            return result != null;
+        }
+    }
+}
