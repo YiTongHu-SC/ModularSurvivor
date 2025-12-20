980c40541ae78ef769c78b7744d74c2c3bfd4946
huyitong <huyitong@merryrealm.com>
2025-12-20 19:00:43 +0800

    Test overlapSystem

---
+++ Assets/Tests/Unit/OverlapSystemTest.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Combat.Systems;
+using Core.Units;
+using NUnit.Framework;
+
+namespace Tests.Unit
+{
+    [TestFixture]
+    public class OverlapSystemTest
+    {
+        private Dictionary<int, UnitData> _allUnits;
+        private UnitOverlapSystem _unitOverlapSystem;
+
+        [SetUp]
+        public void Setup()
+        {
+            _allUnits = new Dictionary<int, UnitData>();
+            _unitOverlapSystem = new UnitOverlapSystem(_allUnits);
+        }
+    }
+}
